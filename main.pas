d105a943a3c5fec8d95f58318fb65258abe8f497
sardap <sardap@users.noreply.github.com>
2016-11-08 15:35:34 +1100

    Add files via upload

--- main.pas
+++ main.pas
@@ -1,31 +1,32 @@
 program PascalBrainFuck;
 
 uses
-  SysUtils;
+  SysUtils, process;
 
 const
-	WRITE_FILE = 'test.pas';
+	DEFUALT_FILE_NAME = 'test.pas';
 
-function HowManyInFile(var fileName: UnicodeString; checkChar: AnsiChar): Integer;
+type
+ Options = record
+  keepFile : Boolean;
+  inputFileName : UnicodeString;
+  outputFileName : UnicodeString;
+  pascalOptions : String;
+ end;
+
+
+function TotalAcco(var code: AnsiString; checkChar: AnsiChar): Integer;
 var
 	i : Integer;
 	temp : String;
-	readingFile : TextFile;
 
 begin
-	AssignFile(readingFile, fileName);
-	Reset(readingFile);
 	result := 0;
-	while not eof(readingFile) do
+	for i:=0 to Length(code) do
 	begin
-		ReadLn(readingFile, temp);
-		for i:=0 to Length(temp) do
-		begin
-			if temp[i] = checkChar then
-				result += 1;
-		end;
+		if code[i] = checkChar then
+			result += 1;
 	end;
-	CloseFile(readingFile);
 end;
 
 function HowManyUntilNext(var toRead: AnsiString; var idx: Integer; toCheck: AnsiChar): Byte;
@@ -56,12 +57,12 @@ begin
 	while i <= Length(toRead) do
 	begin
 		case toRead[i] of
-			'>'	:  WriteLn(writeFile,'	i +=', HowManyUntilNext(toRead, i, '>'), ';');
-			'<'	: WriteLn(writeFile, '	i -=', HowManyUntilNext(toRead, i, '<'), ';');
-			'+'	: WriteLn(writeFile, '	boxs[i] +=', HowManyUntilNext(toRead, i, '+'), ';');
-			'-'	: WriteLn(writeFile, '	boxs[i] -=', HowManyUntilNext(toRead, i, '-'), ';');
+			'>'	:  WriteLn(writeFile,'	i += ', HowManyUntilNext(toRead, i, '>'), ';');
+			'<'	: WriteLn(writeFile, '	i -= ', HowManyUntilNext(toRead, i, '<'), ';');
+			'+'	: WriteLn(writeFile, '	boxs[i] += ', HowManyUntilNext(toRead, i, '+'), ';');
+			'-'	: WriteLn(writeFile, '	boxs[i] -=' , HowManyUntilNext(toRead, i, '-'), ';');
 			'.'	: WriteLn(writeFile, '	Write(Chr(boxs[i]));');
-			','	: WriteLn(writeFile, '	Read(boxs[i]);');
+			','	: WriteLn(writeFile, '	boxs[i] := Byte(ReadKey);');
 			'['	: begin
 				 			WriteLn(writeFile, '	while(boxs[i] <> 0) do');
 							WriteLn(writeFile, '	begin');
@@ -72,70 +73,113 @@ begin
 	end;
 end;
 
-procedure ReadFile(fileName: UnicodeString);
+procedure ReadFile(var inputFileName, outputFileName: UnicodeString);
 var
 	readingLine, brainFuckCode: AnsiString;
 	readingFile : TextFile;
 	writeFile : TextFile;
-	writeFileName : UnicodeString;
 
 begin
-	writeFileName := 'test.pas';
-	WriteLn('Creating ', writeFileName);
-	AssignFile(writeFile, writeFileName);
-	Rewrite(writeFile);
-	WriteLn(writeFile, 'program test;');
-	WriteLn(writeFile, 'uses');
-	WriteLn(writeFile, '	SysUtils;');
-	WriteLn(writeFile, 'var');
-	WriteLn(writeFile, '	boxs: array [0..', HowManyInFile(fileName, '>'), '] of Byte;');
-	WriteLn(writeFile, '	i : Integer;');
-	WriteLn(writeFile, 'begin');
-	WriteLn(writeFile, '	i:=0;');
-	WriteLn(writeFile, '	for i:=0 to High(boxs) do');
-	WriteLn(writeFile, '		boxs[i] := 0;');
-	WriteLn(writeFile, '	i := 0;');
-
-	AssignFile(readingFile, fileName);
+  WriteLn('UnbrainFucking ', inputFileName);
+	AssignFile(readingFile, inputFileName);
 	Reset(readingFile);
 	while not eof(readingFile) do
 	begin
 		ReadLn(readingFile, readingLine);
 		brainFuckCode := brainFuckCode + readingLine;
 	end;
+  CloseFile(readingFile);
+  WriteLn('Creating ', outputFileName);
+  AssignFile(writeFile, outputFileName);
+	Rewrite(writeFile);
 	DeleteGarbage(brainFuckCode, 0);
+  WriteLn(writeFile, 'program test;');
+	WriteLn(writeFile, 'uses');
+	WriteLn(writeFile, '	SysUtils, Crt;');
+	WriteLn(writeFile, 'var');
+	WriteLn(writeFile, '	boxs: array [0..', TotalAcco(brainFuckCode, '>'), '] of Byte;');
+	WriteLn(writeFile, '	i : Integer;');
+	WriteLn(writeFile, 'begin');
+	WriteLn(writeFile, '	i:=0;');
+	WriteLn(writeFile, '	for i:=0 to High(boxs) do');
+	WriteLn(writeFile, '		boxs[i] := 0;');
+	WriteLn(writeFile, '	i := 0;');
 	ReadBrainFuckCode(brainFuckCode, writeFile);
 	WriteLn(writeFile, 'end.');
-	CloseFile(readingFile);
 	CloseFile(writeFile);
 	WriteLn('Read ', Length(brainFuckCode), ' Chars');
 end;
 
-function CheckInput(var fileName: UnicodeString): Boolean;
+function CheckInput(var compilerOptions: Options): Boolean;
+var
+  idx: Byte;
+
 begin
+  idx := 1;
 	result := false;
-	if ParamCount = 1 then
+	if ParamCount >= 1 then
 	begin
-		fileName := ParamStr(1);
-		if FileExists(fileName) then
+    if ParamStr(idx) = '-k' then
+    begin
+      compilerOptions.keepFile := true;
+      idx += 1;
+    end
+    else
+      compilerOptions.keepFile := false;
+		compilerOptions.inputFileName := ParamStr(idx);
+    idx += 1;
+		if FileExists(compilerOptions.inputFileName) then
 		begin
-			WriteLn('UnbrainFucking ', fileName);
-			result := true;
+			WriteLn('Selected File ', compilerOptions.inputFileName);
+      if ParamStr(idx) = '-na'  then
+      begin
+        idx += 1;
+        compilerOptions.outputFileName := ParamStr(idx);
+      end
+      else
+        WriteLn(Pos('.', compilerOptions.inputFileName));
+        compilerOptions.outputFileName := Copy(compilerOptions.inputFileName, 0, Pos('.', compilerOptions.inputFileName)) + 'pas';
+      if (compilerOptions.outputFileName <> compilerOptions.inputFileName) then
+        result := true
+      else
+        WriteLn('Input Name Cannot be the same as Output Name');
 		end
-		else
-			WriteLn(fileName, ' Not Found');
+    else
+			WriteLn(compilerOptions.inputFileName, ' Not Found');
 	end
 	else
 		WriteLn('Must Select Input File');
 end;
 
-procedure main();
+procedure CompilePascalCode(const compilerOptions: Options);
 var
-	fileName : UnicodeString;
+  terminalOut: AnsiString;
 
 begin
-	if CheckInput(fileName) then
-		ReadFile(fileName);
+  WriteLn('Compiling Pascal Code from ', compilerOptions.outputFileName);
+  {$IFDEF WINDOWS}
+		RunCommand('c:\windows\system32\cmd.exe', ['/c', 'fpc -S2 ', compilerOptions.outputFileName], terminalOut);
+	{$ENDIF}
+  {$IFDEF UNIX}
+    RunCommand('/bin/bash', ['-c', 'fpc -S2 ', compilerOptions.pascalOptions, compilerOptions.outputFileName], terminalOut);
+  {$ENDIF}
+  WriteLn(terminalOut);
+end;
+
+procedure main();
+var
+	compilerOptions : Options;
+
+begin
+	if CheckInput(compilerOptions) then
+		ReadFile(compilerOptions.inputFileName, compilerOptions.outputFileName);
+  if FileExists(compilerOptions.outputFileName) then
+    CompilePascalCode(compilerOptions);
+  if (compilerOptions.keepFile = false) then
+  begin
+    DeleteFile(compilerOptions.outputFileName);
+    DeleteFile(Copy(compilerOptions.inputFileName, 0, Pos('.', compilerOptions.inputFileName)) + 'o');
+  end;
 end;
 
 begin
